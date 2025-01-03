7c26470b580e422bb1d462fc03fc9c722c1b3fdb
Jake Loar <jl293@uakron.edu>
2025-01-03 13:18:21 -0500

    Completed chapter 7, deleted chapter 6 due to no functions

--- LubricantCharacterization.py
+++ LubricantCharacterization.py
@@ -1,1 +1,10 @@
 # Chapter 7
+
+def ViscosityIndex(L: float, U: float, H: float): # page 107
+    '''
+    ----------------------------------------------------------------------------------------------------
+    The rate of decrease of lubricant viscosity with increasing temperature is critical to its performance as a lubricant. This behavior is characterized by the viscosity index or VO. A good lubricant has a high VI which means that its viscosity decreases slowly with temperature.
+
+    VI is calculated based upon viscosities measured at 40°C and 100°C. Reference the ASTM standard for VI to determine your lubricant's L and H values in units of cSt.
+    '''
+    return (L-U)/(L-H) * 100
--- main.py
+++ main.py
@@ -3,15 +3,16 @@ Table of Contents
 ----------------------------------------------------------------------------------------------------
 * indicates chapter has been worked through
 ! indicates chapter is in progress
+X indicates no functions provided
 ----------------------------------------------------------------------------------------------------
 Chapter 1: Fundamental Concepts *
 Chapter 2: Solid Materials *
 Chapter 3: Surface Roughness *
 Chapter 4: Non-Conformal Contact *
 Chapter 5: Liquid Properties *
-Chapter 6: Lubricant Composition
-Chapter 7: Lubricant Characterization
-Chapter 8: Lubricant of Conformal Contacts
+Chapter 6: Lubricant Composition X
+Chapter 7: Lubricant Characterization *
+Chapter 8: Lubricant of Conformal Contacts !
 Chapter 9: Lubrication of Non-Conformal Contacts
 Chapter 10: Dry and Mixed Friction
 Chapter 11: Wear

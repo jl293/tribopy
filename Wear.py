d3cce261aef102077710eace28a4152b0ac52a77
Jake Loar <jl293@uakron.edu>
2025-01-08 16:14:26 -0500

    Working Ch. 11

--- Wear.py
+++ Wear.py
@@ -1,1 +1,18 @@
 # Chapter 11
+
+import math
+
+def Archard(K: float, W: float, L: float, H: float):
+    '''
+    ----------------------------------------------------------------------------------------------------
+    By far the most widely-used model for steady-state wear is the Archard equation, V = K * (WL)/H
+
+    K: wear coefficient
+    W: load
+    L: sliding distance, enter 0 if calculating V/L = K * W/H
+    H: hardness of the softer material
+    '''
+    if L != 0:
+        return K*((W*L)/H)
+    elif L == 0:
+        return K*(W/H)
